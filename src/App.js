254926a4c8e4f17a17ea29239cec468b3979017a
Chris Chun <4michael1angelo5@gmail.com>
2019-11-24 14:31:31 -0800

    hello world

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 import './buttons.scss'
 import Home from './components/homeComponent'
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 
 function App() {
   return (
-    <BrowserRouter>
+   
     <div className="App">
       <Home/>
     </div>
-    </BrowserRouter>
+    
   );
 }
 
